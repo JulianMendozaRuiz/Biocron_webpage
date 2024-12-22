87bd013d41e900bfdb08061b012c81fff9f35559
Julian Mendoza <jdmendoza_90@hotmail.com>
2024-12-22 14:37:44 -0500

    Added bullets and description to industries. Missing change of tabs and mask in description of industry

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,29 @@ const routes: Routes = [
     loadChildren: () =>
       import('./contact/contact.module').then((m) => m.ContactModule),
   },
-  { path: 'nosotros', loadChildren: () => import('./about-us/about-us.module').then(m => m.AboutUsModule) },
-  { path: 'servicios', loadChildren: () => import('./services/services.module').then(m => m.ServicesModule) },
-  { path: 'industrias', loadChildren: () => import('./industries/industries.module').then(m => m.IndustriesModule) },
-  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+  {
+    path: 'nosotros',
+    loadChildren: () =>
+      import('./about-us/about-us.module').then((m) => m.AboutUsModule),
+  },
+  {
+    path: 'servicios',
+    loadChildren: () =>
+      import('./services/services.module').then((m) => m.ServicesModule),
+  },
+  {
+    path: 'industrias',
+    loadChildren: () =>
+      import('./industries/industries.module').then((m) => m.IndustriesModule),
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/industries/team/team.component.ts
+++ src/app/industries/team/team.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'comp-team',
   templateUrl: './team.component.html',
-  styleUrl: './team.component.scss'
+  styleUrl: './team.component.scss',
 })
-export class TeamComponent {
+export class TeamComponent implements OnInit {
+  @Input() content: any;
 
+  title: string | null = null;
+  description: string | null = null;
+
+  ngOnInit(): void {
+    this.title = this.content.team.title;
+    this.description = this.content.team.description;
+  }
 }
